409135d719de49b617a0c2846a2143e4d689ccd7
Arch Linux J <jdavis3695@gmail.com>
2024-03-28 19:56:34 -0700

    update

--- init.lua
+++ init.lua
@@ -546,8 +546,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
+        html = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:

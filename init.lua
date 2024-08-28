98f314474cb920794535f80b386a9a3d569f22a9
JackAvis <Dev@Jackavis.com>
2024-08-28 16:18:04 -0700

    Fix which-key

--- init.lua
+++ init.lua
@@ -287,21 +287,21 @@ require('lazy').setup({
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
+    ops = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
 
       -- Document existing key chains
       require('which-key').register {
-        { '<leader>c', group = '[C]ode' },
-        { '<leader>c_', hidden = true },
-        { '<leader>d', group = '[D]ocument' },
-        { '<leader>d_', hidden = true },
-        { '<leader>r', group = '[R]ename' },
-        { '<leader>r_', hidden = true },
-        { '<leader>s', group = '[S]earch' },
-        { '<leader>s_', hidden = true },
-        { '<leader>w', group = '[W]orkspace' },
-        { '<leader>w_', hidden = true },
+        { '', group = '[C]ode' },
+        { '', desc = '<leader>r_', hidden = true },
+        { '', group = '[S]earch' },
+        { '', desc = '<leader>s_', hidden = true },
+        { '', group = '[W]orkspace' },
+        { '', group = '[R]ename' },
+        { '', group = '[D]ocument' },
+        { '', desc = '<leader>d_', hidden = true },
+        { '', desc = '<leader>c_', hidden = true },
+        { '', desc = '<leader>w_', hidden = true },
       }
     end,
   },
